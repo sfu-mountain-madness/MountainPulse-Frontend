52e36b7a9c833f571464f279ff5f6d782a5a00e4
VertexC <bob2420083992@gmail.com>
2018-02-17 13:51:07 -0800

    chart doesn't work

--- src/components/Chart.vue
+++ src/components/Chart.vue
@@ -5,16 +5,18 @@
 </template>
 
 <script>
+import gql from 'graphql-tag'
 import echarts from 'echarts'
 // import Echarts modulde to reduce the bundle size
 import 'echarts/lib/chart/bar'
 import 'echarts/lib/component/tooltip'
 
 export default {
-  name: 'Chart.vue',
+  name: 'Chart',
   data() {
     return {
       weatherChart: null,
+      weatherLimit: 10,
       weatherConfig: {
         title: {
           text: 'Weather'
@@ -44,7 +46,36 @@ export default {
   mounted: function () {
     this.weatherChart = echarts.init(document.getElementById('weatherData'))
     this.weatherChart.setOption(this.weatherConfig)
+  },
+  methods: {},
+  apollo: {
+    weather: {
+      query: gql`query Weather($limit: Int!){
+        weather(limit:$limit){
+          temp,
+          date
+        }
+      }`,
+      variables() {
+        return {
+          limit: this.weatherLimit
+        }
+      },
+      update: function (data) {
+        const temp = data.weather.map(item => {
+          return [item.date, item.temp]
+        })
+        this.weatherConfig.series[0].data = temp
+        console.log(temp)
+        // if (this.weatherChart) {
+        //   this.weatherChart.setOption(this.weatherConfig)
+        // }
+        this.weatherChart.setOption(this.weatherConfig)
+        return data.weather
+      }
+    }
   }
+
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -4,15 +4,43 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuetify from 'vuetify'
+
+// for Apollo
+import {
+  ApolloClient
+} from 'apollo-client'
+import {
+  HttpLink
+} from 'apollo-link-http'
+import {
+  InMemoryCache
+} from 'apollo-cache-inmemory'
+import VueApollo from 'vue-apollo'
+// Apollo object
+const httpLink = new HttpLink({
+  uri: 'https://backend.haoxp.xyz/graphql'
+})
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient
+})
+
+// CSS
 import('vuetify/dist/vuetify.min.css')
 
 Vue.use(Vuetify)
+Vue.use(VueApollo)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  apolloProvider,
   components: { App },
   template: '<App/>'
 })
